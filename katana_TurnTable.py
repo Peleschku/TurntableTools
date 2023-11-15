7bb0a1ec0557ab35a83ab0c2f80625ceea8809d8
adelepeleschka@hotmail.co.uk <133109390+Peleschku@users.noreply.github.com>
2023-11-15 23:16:10 +0000

    layout rejig

--- katana_TurnTable.py
+++ katana_TurnTable.py
@@ -10,37 +10,83 @@ class turntableMainWindow(QWidget):
         self.createWindow()
     
     def createWindow(self):
+        
 
         self.setGeometry(150, 150, 200, 250)
         self.setWindowTitle("Turn Table Creator")
 
+        self.populateUI()
+        self.show()
+    
+    def populateUI(self):
+
         layout = QGridLayout()
+        threePointLayout = QGridLayout()
+        skyDomeLayout = QVBoxLayout()
+        studioLayout = QGridLayout()
 
         self.assetLabel = QLabel("Path to Asset")
         self.assetPath = QLineEdit()
 
         self.searchButton = QPushButton("Search")
         self.searchButton.clicked.connect(self.assetSearch)
+        
+        '''
+        Tabs to specify lighting setups and the like
+        '''
+        self.lightingTabs = QTabWidget()
 
+        # three point lighting layouts
+
+        self.threePointTab = QWidget()
+
+        self.enableThreepoint = QCheckBox("Enable Three Point Lighting")
+        
         self.threePoint = QPushButton("Three Point Lighting")
         self.threePoint.clicked.connect(self.threePointSetup)
+
+        threePointLayout.addWidget(self.threePoint, 0, 1)
+        self.threePointTab.setLayout(threePointLayout)
+
+        # skydome lighting setup
         
+        self.skyDomeTab = QWidget()
+
         self.skyDome = QPushButton("Sky Dome")
         self.skyDome.clicked.connect(self.skyDomeSetup)
 
+        skyDomeLayout.addWidget(self.skyDome)
+        self.skyDomeTab.setLayout(skyDomeLayout)
+
+        # studio lighting setup
+
+        self.studioLightingTab = QWidget()
+        
         self.studio = QPushButton("Studio Lighting")
         self.studio.clicked.connect(self.studioSetup)
+
+        studioLayout.addWidget(self.studio, 0, 1)
+
+        self.studioLightingTab.setLayout(studioLayout)
+
+        # adding the tabs to the overall tab widget
+
+        self.lightingTabs.addTab(self.threePointTab, "Three Point Setup")
+        self.lightingTabs.addTab(self.skyDomeTab, "Sky Dome Setup")
+        self.lightingTabs.addTab(self.studioLightingTab, "Studio Lighting Setup")
+
+        self.createScene = QPushButton("Create Turntable Scene!")
+        self.createScene.clicked.connect(self.generateTurnTable)
         
 
 
         
         
         layout.addWidget(self.assetLabel, 0, 0)
-        layout.addWidget(self.assetPath, 0, 1)
-        layout.addWidget(self.searchButton, 0, 2)
-        layout.addWidget(self.threePoint, 1, 0)
-        layout.addWidget(self.skyDome, 1, 1)
-        layout.addWidget(self.studio, 1, 2)
+        layout.addWidget(self.assetPath, 0, 1, 1, 3)
+        layout.addWidget(self.searchButton, 0, 4)
+        layout.addWidget(self.lightingTabs, 1, 0, 1, 5)
+        layout.addWidget(self.createScene, 2, 3)
 
         self.show()
         self.setLayout(layout)
@@ -50,6 +96,22 @@ class turntableMainWindow(QWidget):
 
         if self.filePath:
             self.assetPath.insert(self.filePath[0])
+    
+    def threePointSetup(self):
+        print('Three Point!')
+    
+    def skyDomeSetup(self):
+        print('Sky Dome!')
+    
+    def studioSetup(self):
+        print("Studio!")
+    
+    def generateTurnTable(self):
+        print("Yee Haw!")
+
+    
+
+    
 
 
 
