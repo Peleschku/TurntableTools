77a8fb7b876d75b54c2f61c621e43bb5cffb409f
Adele <adelepeleschka@hotmail.co.uk>
2023-11-28 18:33:35 +0000

    Hooking things up
    Super basic UI is set up, and when the button is pressed you get a super basic turntable setup.
    Next steps are:
    - Figure out how to get textures applied to the skydome that's created
    - try to reintroduce the tabs system on the wireframe. Should hopefully be not too bad now that I have functions for making lights, and have figures out how to share things between classes.

--- turnTable_V1.py
+++ turnTable_V1.py
@@ -39,6 +39,10 @@ class TurntableWindow(QWidget):
         updateCamRes = QLabel("Resolution")
         self.cameraResSelect = UI4.Widgets.ResolutionComboBox(self)
 
+        # HDRI setup
+
+        self.skydomeSetup = SkydomeSetup()
+
         
         # button that assembles the turn table when clicked
 
@@ -46,13 +50,14 @@ class TurntableWindow(QWidget):
         self.createTurnTable.clicked.connect(self.generateTT)
 
         layout.addWidget(assetLabel, 0, 0)
-        layout.addWidget(self.assetPath, 0, 1)
-        layout.addWidget(self.searchButton, 0, 4)
+        layout.addWidget(self.assetPath, 0, 1, 1, 4)
+        layout.addWidget(self.searchButton, 0, 5)
         layout.addWidget(cameraHeader, 1, 0)
         layout.addWidget(self.createCamera, 2, 0)
         layout.addWidget(updateCamRes, 3, 0)
-        layout.addWidget(self.cameraResSelect, 3, 1)
-        layout.addWidget(self.createTurnTable, 4, 0, 1, 4)
+        layout.addWidget(self.cameraResSelect, 3, 1, 1, 4)
+        layout.addWidget(self.skydomeSetup, 4, 0, 1, 5)       
+        layout.addWidget(self.createTurnTable, 5, 0, 1, 5)
 
         self.show()
         self.setLayout(layout)
@@ -101,18 +106,79 @@ class TurntableWindow(QWidget):
         camAssetMerge = self.multiMerge([alembicCreate, camera])
         camMergeOut = camAssetMerge.getOutputPort('out')
 
+        # if the 'use skydome' check box is checked, create a skydome
+        # note to self: the line in the if statement is calling a function
+        # from within the SkydomeSetup class
+        
+        if self.skydomeSetup.useSkydome.isChecked():
+            skydome = SkydomeSetup.createSkydome(self)
+
+        skydomeInput = skydome.getInputPort('in')
+        skydomeOutput = skydome.getOutputPort('out')
+
+        camMergeOut.connect(skydomeInput)
+
         # creates render settings node and assigns the camera resolution
         # based on the selection in the UI's dropdown
-        
+
         renderSettings = NodegraphAPI.CreateNode('RenderSettings', self.root)
         resolution = UI4.FormMaster.CreateParameterPolicy(None, renderSettings.getParameter('args.renderSettings.resolution'))
         resolution.setValue(str(self.cameraResSelect.currentText()))
 
         renderInput = renderSettings.getInputPort('input')
+        skydomeOutput.connect(renderInput)
 
-        camMergeOut.connect(renderInput)
+class SkydomeSetup(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.parentLayout = QGridLayout()
+        self.root = NodegraphAPI.GetRootNode()
+        self.createModule()
+    
+    def createModule(self):
+        skydomeHeader = QLabel("Skydome Setup")
+        useTexture = QCheckBox("Use HDRI Texture?")
+
+        texturePathLabel = QLabel("Path to Texture")
+        self.texturePath = QLineEdit()
+        self.searchTexture = QPushButton("Search")
+        self.searchTexture.clicked.connect(self.textureBrowser)
 
 
+       
+        self.useSkydome = QCheckBox("Use Skydome Setup")
+
+        self.parentLayout.addWidget(skydomeHeader, 0, 0)
+        self.parentLayout.addWidget(useTexture, 1, 0)
+        self.parentLayout.addWidget(texturePathLabel, 2, 0)
+        self.parentLayout.addWidget(self.texturePath, 2, 1, 1, 4)
+        self.parentLayout.addWidget(self.searchTexture, 2, 5)
+        self.parentLayout.addWidget(self.useSkydome, 3, 5)
+
+        self.show()
+        self.setLayout(self.parentLayout)
+    
+    def createSkydome(self):
+        gafferThree = NodegraphAPI.CreateNode("GafferThree", self.root)
+        rootPackage = gafferThree.getRootPackage()
+        rootPackage.createChildPackage('EnvironmentLightPackage', 'envLight')
+
+        light = rootPackage.getChildPackage('envLight')
+        lightMatNode = light.getMaterialNode()
+        lightShader = lightMatNode.addShaderType('dlEnvironmentShader')
+
+        setEnvLight = UI4.FormMaster.CreateParameterPolicy(None, lightMatNode.getParameter('shaders.dlEnvironmentShader'))
+        setEnvLight.setValue("environmentLight")
+        lightMatNode.checkDynamicParameters()
+
+        return gafferThree
+    
+    def textureBrowser(self):
+                
+        self.filePath = QFileDialog.getOpenFileName(self, "Select Asset",)
+
+        if self.filePath:
+            self.texturePath.insert(self.filePath[0])
 
 
 
