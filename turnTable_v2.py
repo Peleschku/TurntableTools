c53652f389509e5d0bd2d22c253a465ea09ccae4
Adele <adelepeleschka@hotmail.co.uk>
2023-12-05 12:42:23 +0000

    Update turnTable_v2.py
    refactoring things so that the lookdev assets work from the main turntable window

--- turnTable_v2.py
+++ turnTable_v2.py
@@ -4,6 +4,110 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+def geoCreate(primitiveType, parent):
+    primitiveCreate = NodegraphAPI.CreateNode('PrimitiveCreate', parent)
+    changePrimType = UI4.FormMaster.CreateParameterPolicy(None, primitiveCreate.getParameter('type'))
+    changePrimType.setValue(primitiveType, 0)
+
+    return primitiveCreate
+
+def shadingNodeCreate(nodeType, parent, dlShadingNodeType = 'DlShadingNode'):
+    dlPrincipled = NodegraphAPI.CreateNode(dlShadingNodeType, parent)
+    dlPrincipled.getParameter('nodeType').setValue(nodeType, 0)
+    dlPrincipled.getParameter('name').setValue(nodeType, 0)
+    dlPrincipled.checkDynamicParameters()
+    
+    return dlPrincipled
+
+
+def materialAssignSetup(assetLocation, material, parent):
+    
+    materialAssign = NodegraphAPI.CreateNode('MaterialAssign', parent)
+    materialAssign.getParameter('CEL').setValue(assetLocation, 0)
+    materialAssign.getParameter('args.materialAssign.enable').setValue(1, 0)
+    materialAssign.getParameter('args.materialAssign.value').setValue(material, 0)
+
+    return materialAssign
+
+
+def groupNodeSetup(parent):
+    group = NodegraphAPI.CreateNode('Group', parent)
+    group.addOutputPort('groupOut')
+    groupReturn = group.getReturnPort('out')
+
+    return group
+
+# ---------------------------------------------------------------------------------------------------
+# ------------------------------ Functions for connecting nodes together ----------------------------
+# ---------------------------------------------------------------------------------------------------
+
+def nmcConnect(networkMaterial, shadingNode, terminalInput):
+
+    if networkMaterial.getType() == 'NetworkMaterialCreate':
+        terminal = networkMaterial.getNetworkMaterials()[0]
+        terminalIn = terminal.getInputPort(terminalInput)
+    elif networkMaterial.getType() == 'NetworkMaterial':
+        terminalIn = networkMaterial.addInputPort(terminalInput)
+    
+    shadingNodeOut = shadingNode.getOutputPort('outColor')
+    terminalIn.connect(shadingNodeOut)
+
+    print(networkMaterial.getParameter('NodeType') == 'NetworkMaterialCreate')
+
+
+def connectTwoNodes(nodeOutput, nodeInput, outValue, inValue):
+
+    nodeOutPort = nodeOutput.getOutputPort(outValue)
+    nodeInPort = nodeInput.getInputPort(inValue)
+
+    nodeOutPort.connect(nodeInPort)
+    
+
+def multiMerge (nodesToMerge, parent):
+    mergeNode = NodegraphAPI.CreateNode('Merge', parent)
+
+    for node in nodesToMerge:
+        outputPort = node.getOutputPort('out')
+        mergeInput = mergeNode.addInputPort('i')
+        outputPort.connect(mergeInput)
+    return mergeNode
+
+# ---------------------------------------------------------------------------------------------------
+# --------------------------------- Additional helpful functions ------------------------------------
+# ---------------------------------------------------------------------------------------------------
+
+def getMaterialPath(nmc):
+
+    nmcRootLocation = nmc.getParameterValue('rootLocation', NodegraphAPI.GetCurrentTime())
+    
+    material = nmc.getNetworkMaterials()[-1]
+    materialName = material.getParameterValue('name', NodegraphAPI.GetCurrentTime())
+
+    materialPath = nmcRootLocation + "/" + materialName
+
+    return materialPath
+
+def subDivideMesh( meshToSubdivide, parent):
+    attributeSet = NodegraphAPI.CreateNode('AttributeSet', parent)
+    meshLocation = meshToSubdivide.getParameterValue('name', NodegraphAPI.GetCurrentTime())
+    path = UI4.FormMaster.CreateParameterPolicy(None, attributeSet.getParameter('paths.i0'))
+    path.setValue(meshLocation)
+
+    attributeName = UI4.FormMaster.CreateParameterPolicy(None, attributeSet.getParameter('attributeName'))
+    attributeName.setValue('Type')
+
+    attributeType = UI4.FormMaster.CreateParameterPolicy(None, attributeSet.getParameter('attributeType'))
+    attributeType.setValue('string')
+
+    subdivideMesh = UI4.FormMaster.CreateParameterPolicy(None, attributeSet.getParameter('stringValue.i0'))
+    subdivideMesh.setValue('subdmesh')
+
+    return attributeSet
+
+
+
+
+
 class TurntableWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -42,6 +146,10 @@ class TurntableWindow(QWidget):
 
         self.cameraSetup = CameraSettings()
 
+        # adding the LookDev Layout
+
+        self.lookDevSetup = LookDevSetup()
+
         # create turntable button
         self.createTurnTable = QPushButton("Create Turn Table")
         self.createTurnTable.clicked.connect(self.generateTT)
@@ -51,10 +159,11 @@ class TurntableWindow(QWidget):
         mainLayout.addWidget(assetLabel, 0, 0, 1, 1)
         mainLayout.addWidget(self.assetPath, 0, 1, 1, 6)
         mainLayout.addWidget(self.searchButton, 0, 7, 1, 1)
-        # adding the skydome layout
+        # adding additional widgets
         mainLayout.addWidget(self.skydomeSetup, 1, 0, 1, 8)
         mainLayout.addWidget(self.cameraSetup, 2, 0, 1, 8)
-        mainLayout.addWidget(self.createTurnTable, 3, 0, 1, 8)
+        mainLayout.addWidget(self.lookDevSetup, 3, 0, 1, 8)
+        mainLayout.addWidget(self.createTurnTable, 4, 0, 1, 8)
         self.show()
         self.setLayout(mainLayout)
 
@@ -104,6 +213,19 @@ class TurntableWindow(QWidget):
 
     
     def generateTT(self):
+
+        if self.lookDevSetup.enableGrey.isChecked() or self.lookDevSetup.enableChrome.isChecked() or self.lookDevSetup.enableChart.isChecked() or self.lookDevSetup.enableAll.isChecked() == True:
+            networkMaterialCreate = NodegraphAPI.CreateNode('NetworkMaterialCreate', self.root)
+            nmcTerminal = getMaterialPath(networkMaterialCreate)
+
+        if self.lookDevSetup.enableGrey.isChecked() == True:
+            greySphereSetup = self.lookDevSetup.createGreyBall(networkMaterialCreate, nmcTerminal, self.root)
+        
+        if self.lookDevSetup.enableChrome.isChecked() == True :
+            chromeSphereSetup = self.lookDevSetup.createChromeBall(networkMaterialCreate, nmcTerminal, self.root)
+
+        if self.lookDevSetup.enableGrey.isChecked() and self.lookDevSetup.enableChrome.isChecked() == True:
+            multiSetup = self.lookDevSetup.multiSetup(networkMaterialCreate, self.root)
 
         alembicCreate = NodegraphAPI.CreateNode('Alembic_In', self.root)
         assetSet = UI4.FormMaster.CreateParameterPolicy(None, alembicCreate.getParameter('abcAsset'))
@@ -371,5 +493,140 @@ class CameraSettings(QWidget):
         
         self.show()
         self.setLayout(self.parentLayout)
+
+class LookDevSetup(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.createUI()
+    
+    def createUI(self):
+
+        layout = QGridLayout()
+        layout.setVerticalSpacing(15)
+
+        lookdevHeading = QLabel("Look Development Assets")
+
+        backgrounds = QLabel("Backgrounds")
+
+        self.enablePlatform = QCheckBox("Enable Platform")
+        self.enableBackdrop = QCheckBox("Backdrop")
+
+        lookdevTools = QLabel("LookDev Tools")
+
+        self.enableGrey = QCheckBox("Enable Grey Ball")
+        self.enableChrome = QCheckBox("Enable Chrome Ball")
+        self.enableChart = QCheckBox("Enable Macbeth Chart")
+        self.enableAll = QCheckBox("Enable All")
+
+
+        layout.addWidget(lookdevHeading, 0, 0)
+        layout.addWidget(backgrounds, 1, 0)
+        layout.addWidget(self.enablePlatform, 2, 0)
+        layout.addWidget(self.enableBackdrop, 2, 1)
+        layout.addWidget(lookdevTools, 3, 0)
+        layout.addWidget(self.enableGrey, 4, 0)
+        layout.addWidget(self.enableChrome, 4, 1)
+        layout.addWidget(self.enableChart, 4, 2)
+        layout.addWidget(self.enableAll, 5, 1, Qt.AlignCenter)
+
+        self.show()
+        self.setLayout(layout)
+    
+    def createGreyBall(self, nmc, nmcLocation, parent):
+        networkMaterialCreate = nmc
+
+        self.greySphere = geoCreate('poly sphere', parent)
+        greyNameSet = self.greySphere.getParameter('name').setValue('/root/world/geo/greySphere', 0)
+        greySphereLocation = self.greySphere.getParameterValue('name', NodegraphAPI.GetCurrentTime())
+
+        self.greyAttributeSet = subDivideMesh(self.greySphere, parent)
+
+        greyAttributeConnect = connectTwoNodes(self.greySphere, self.greyAttributeSet, 'out', 'A')
+
+        # setting up the material
+        greyMat = shadingNodeCreate('dlPrincipled', networkMaterialCreate)
+        greyBaseColor = UI4.FormMaster.CreateParameterPolicy(None, greyMat.getParameter('parameters.color'))
+        greyBaseColor.setValue([0.18,
+                                0.18,
+                                0.18])
+        greyRoughness = UI4.FormMaster.CreateParameterPolicy(None, greyMat.getParameter('parameters.roughness'))
+        greyRoughness.setValue(1)
+
+        greyConnectInsideNMC = nmcConnect(networkMaterialCreate, greyMat, 'dlSurface')
+
+        self.greyGeoNMCMerge = multiMerge([self.greyAttributeSet, networkMaterialCreate], parent)
+
+        self.greyMaterialAssign = materialAssignSetup(greySphereLocation, nmcLocation, parent)
+        self.greyMaterialIntoAssign = connectTwoNodes(self.greyGeoNMCMerge, self.greyMaterialAssign, 'out', 'input')
+    
+    def createChromeBall(self, nmc, nmcLocation, parent):
+        networkMaterialCreate = nmc
+
+        self.chromeSphere = geoCreate('poly sphere', parent)
+        chromeNameSet = self.chromeSphere.getParameter('name').setValue('/root/world/geo/chromeSphere', 0)
+        chromeSphereLocation = self.chromeSphere.getParameterValue('name', NodegraphAPI.GetCurrentTime())
+
+        self.chromeAttributeSet = subDivideMesh(self.chromeSphere, parent)
+
+        chromeAttributeConnect = connectTwoNodes(self.chromeSphere, self.chromeAttributeSet, 'out', 'A')
+
+        if self.enableGrey.isChecked() == True:
+            chromeNetworkMaterial = NodegraphAPI.CreateNode('NetworkMaterial', nmc)
+            chromeMaterialLocation = '/root/materials/NetworkMaterial1'
+        else:
+            chromeMaterialLocation = nmcLocation
+        
+        chromeMaterial = shadingNodeCreate('dlPrincipled', nmc)
+
+        chromeMaterialColor = UI4.FormMaster.CreateParameterPolicy(None, chromeMaterial.getParameter('parameters.color'))
+        chromeMaterialColor.setValue([1,
+                                      1,
+                                      1])
+        chromeMaterialRoughness = UI4.FormMaster.CreateParameterPolicy(None, chromeMaterial.getParameter('parameters.roughness'))
+        chromeMaterialRoughness.setValue(0)
+        chromeMaterialMetallic = UI4.FormMaster.CreateParameterPolicy(None, chromeMaterial.getParameter('parameters.metallic'))
+        chromeMaterialMetallic.setValue(1)
+        
+
+        if self.enableGrey.isChecked() == True:
+            chromeConnectInsideNMC = nmcConnect(chromeNetworkMaterial, chromeMaterial, 'dlSurface')
+        else:
+            chromeConnectInsideNMC = nmcConnect(nmc, chromeMaterial, 'dlSurface')
+
+        if self.enableGrey != True:
+            self.chromeMaterialMerge = multiMerge([self.chromeAttributeSet, nmc], parent)
+            self.chromeMaterialAssign = materialAssignSetup(chromeSphereLocation, chromeMaterialLocation, parent)
+            chromeMaterialIntoAssign = connectTwoNodes(self.chromeMaterialMerge, self.chromeMaterialAssign, 'out', 'input')
+
+    def multiSetup(self, nmc, parent):
+
+        primGroup = groupNodeSetup(parent)
+
+        self.greySphere.setParent(primGroup)
+        self.greyAttributeSet.setParent(primGroup)
+
+        self.chromeSphere.setParent(primGroup)
+        self.chromeAttributeSet.setParent(primGroup)
+        
+        nmc.setParent(primGroup)
+
+        self.greyGeoNMCMerge.setParent(primGroup)
+        if self.enableGrey.isChecked() and self.enableChrome.isChecked():
+            addMergePort = self.greyGeoNMCMerge.addInputPort('i2')
+            chromeMergeOut = self.chromeAttributeSet.getOutputPort('out')
+
+            addMergePort.connect(chromeMergeOut)
+        
+        nmcOut = self.greyGeoNMCMerge.getOutputPort('out')
+        primGroupReturn = primGroup.getReturnPort('groupOut')
+
+        nmcOut.connect(primGroupReturn)
+
+        groupOut = connectTwoNodes(primGroup, self.greyMaterialAssign, 'groupOut', 'input')
+
+
+
+
+
 
 open = TurntableWindow()
