d5f52316b279f86f1b3c5906a434d26d54cfb00c
Adele Peleschka <adele.peleschka@foundry.com>
2026-04-29 12:28:16 +0100

    removing all, unneeded versions

--- turnTable_v1/OLD/nodes.py
+++
@@ -1,9 +0,0 @@
-def assetCreate(parent, assetPath):
-    alembicIn = NodegraphAPI.CreateNode('AlembicIn', parent)
-    setAssetPath = UI4.FormMaster.CreateParameterPolicy(None, alembicIn.getParameter('abcAsset'))
-    setAssetPath.setValue(str(assetPath))
-
-    return alembicIn
-
-def createCamera(parent, ):
-    
--- turnTable_v1/katanaTurnTable/__init__.py
+++
@@ -1,3 +0,0 @@
-from TurnTable import turntable
-
-
